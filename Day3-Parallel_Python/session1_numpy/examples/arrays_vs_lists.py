59ef46001ab82804d589ce83c1d370d11d008b69
Nick <feathern@colorado.edu>
2017-05-12 13:11:17 -0600

    Modified the Numpy portion of the Python tutorial. Added a few additional examples and added headers to all example programs.

--- Day3-Parallel_Python/session1_numpy/examples/arrays_vs_lists.py
+++ Day3-Parallel_Python/session1_numpy/examples/arrays_vs_lists.py
@@ -1,14 +1,22 @@
+###############################################################################
+#   Example:   Using Numpy arrays instead lists
+#
+#              Using numpy array syntax can speed up a calculation considerably.
+#              We illustrate this by recording the time needed to perform
+#              a = a*2 and c = a*b for lists and Numpy arrays with large
+#              element counts.
+
 import numpy as np
 import time
 
+npts = 10000000    # number of elements in each list/array
+ntrials = 2        # number of times to repeat the calculation
 
-
-
-npts = 10000000
-ntrials = 2
+#Initialize our Numpy arrays
 a = np.zeros(npts)
 b = np.zeros(npts)
 
+#Initialize our lists
 d = []
 e = []
 f = []
@@ -16,9 +24,7 @@ for i in range(npts):
     d.append(0.1)
     e.append(2.0)
     f.append(0.0)
-##############################################################
-# Numpy arrays vs. lists
-# Using numpy array syntax can simply and speedup code considerably...
+
 print(' ')
 print('Timing array vs. list operations for arrays having '+str(npts)+' elements...')
 
