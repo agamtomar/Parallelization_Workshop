1789f8a573869ad604358ce43f9cfa6ec2ca2ef3
Nick <feathern@colorado.edu>
2017-05-15 10:48:34 -0600

    Trapezoid integration exercises for session 3 are complete

--- Day3-Parallel_Python/session3_mpi4py/exercises/solutions/trapezoid_sol.py
+++ Day3-Parallel_Python/session3_mpi4py/exercises/solutions/trapezoid_sol.py
@@ -1,4 +1,8 @@
-#!/usr/bin/env python
+#######################################################3
+#
+#       Solution:  Trapezoidal Integration
+#
+#
 def myfunc(x):
     val = x*x*x
     return val
@@ -26,6 +30,7 @@ def main():
     from mpi4py import MPI
     import sys
     import numpy
+    import time
 
     num_proc  = MPI.COMM_WORLD.Get_size()
     my_rank   = MPI.COMM_WORLD.Get_rank()
@@ -37,32 +42,44 @@ def main():
     comm.Barrier()
 
 
-    ntests = 10000
-    ntrap = 1000000/num_proc  # Each rank gets 1000,000/num_proc trapezoids
+    ntests = 100
+    ntrap = 1000000//num_proc  # Each rank gets 1000,000/num_proc trapezoids
 
-    ntests = 2  #comment this line out once your code is working correctly
+
 
 
     xone = 1.0  # The global limits of integration
     xtwo = 2.0
 
     # Each rank should integrate between a unique pair of values myxone and myxtwo
-    # What should deltax and myxone be to make this work?
     deltax = (xtwo-xone)/num_proc
     myxone = xone+deltax*my_rank
     myxtwo = myxone+deltax
 
-    local_integral = numpy.array(0.0,'d') 
-    global_integral = numpy.array(0.0, 'd')
+    local_integral  = numpy.ndarray(1, dtype='float64') 
+    global_integral = numpy.ndarray(1, dtype='float64')
+    t0 = time.time()
     for i in range(ntests):
 
-        local_integral = 0*local_integral+trapezoid_int(myxone,myxtwo,ntrap)
+        local_integral[0] = trapezoid_int(myxone,myxtwo,ntrap)
         # The call to Allreduce will sum the value of local_integral across
         # all processes, and store it in global_integral        	
         comm.Allreduce([local_integral, MPI.DOUBLE], [global_integral,MPI.DOUBLE], op=MPI.SUM)
 
+    t1 = time.time()
     sys.stdout.write("  Rank %d contributes %f to the global integral value of %f.\n" 
                      % (my_rank, local_integral, global_integral))
+    sys.stdout.flush()
+
+    dt = t1-t0
+    dt_local =  numpy.ndarray(1,dtype='float64')
+    dt_local[0] = dt
+    dt_global = numpy.ndarray(1,dtype='float64')
+
+    comm.Allreduce([dt_local, MPI.DOUBLE], [dt_global,MPI.DOUBLE], op=MPI.SUM)
+    dt_avg = (dt_global/num_proc)/ntests
+    if (my_rank == 0):
+        sys.stdout.write('  Average integration time for %d MPI ranks is %f seconds.\n' % (num_proc, dt_avg[0]))
     MPI.Finalize()
 main()
 
